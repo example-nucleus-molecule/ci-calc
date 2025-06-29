e59e43ce7975a10f425b741cc50d6b00c4f40120
Paul Williams <218377652+example-nucleus-molecule@users.noreply.github.com>
2025-06-29 17:39:54 +1000

    Implement periodic calculator
    At present only working for monthly/quarterly/annual frequency, will need to handle interest payment at maturity differently.

--- internal/cicalc.go
+++ internal/cicalc.go
@@ -1,18 +1,25 @@
 // Package cicalc implements a compound interest calculator.
 package cicalc
 
+import (
+	"math"
+)
+
 type Frequency int
 
 const (
-	Monthly Frequency = iota
-	Quarterly
-	Annually
-	AtMaturity
+	Monthly    Frequency = 12
+	Quarterly  Frequency = 4
+	Annual     Frequency = 1
+	AtMaturity Frequency = 0
 )
 
-// Periodic takes a principal amount, an interest rate, a term (in years), and
-// an interest frequency, and returns a final amount (rounded to the nearest
-// whole number).
-func Periodic(principal int, rate float32, term int, freq Frequency) int {
-	return 0
+// Periodic takes a principal amount, an interest rate (in percentage points,
+// e.g. 1.25 for 1.25%), a term (in years), and an interest frequency, and
+// returns a final amount (rounded to the nearest whole number).
+func Periodic(
+	principal float64, rate float64, term float64, freq Frequency,
+) int {
+	floatFreq := float64(freq)
+	return int(math.Round(principal * math.Pow(1+((rate/100)/floatFreq), term*floatFreq)))
 }
--- internal/cicalc_test.go
+++ internal/cicalc_test.go
@@ -5,19 +5,40 @@ import "testing"
 func TestPeriodic(t *testing.T) {
 	testCases := []struct {
 		name      string
-		principal int
-		rate      float32
-		term      int
+		principal float64
+		rate      float64
+		term      float64
 		freq      Frequency
 		expected  int
 	}{
 		{
-			name:      "success",
+			name:      "with monthly frequency",
+			principal: 10000,
+			rate:      5,
+			term:      5,
+			freq:      Monthly,
+			expected:  12834,
+		}, {
+			name:      "with quarterly frequency",
 			principal: 10000,
 			rate:      5,
 			term:      5,
 			freq:      Quarterly,
 			expected:  12820,
+		}, {
+			name:      "with annual frequency",
+			principal: 10000,
+			rate:      5,
+			term:      5,
+			freq:      Annual,
+			expected:  12763,
+		}, {
+			name:      "with atMaturity frequency",
+			principal: 10000,
+			rate:      5,
+			term:      5,
+			freq:      AtMaturity,
+			expected:  12500,
 		},
 	}
 
